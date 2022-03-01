371ed84cf4c7808587b7b141278c4a15c019fa02
Pierre Coppermann <pcoppy@gmail.com>
2022-03-01 15:51:57 +0100

    create participant controller

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -1,2 +1,30 @@
 class ParticipantsController < ApplicationController
+  def new
+    @participant = Participant.new
+    authorize @participant
+  end
+
+  def create
+    @trip = Trip.find(params[:trip_id])
+    @participant = Participant.new(participant_params)
+    @participant.save
+    authorize @participant
+    if @partcipant.save
+      redirect_to trip_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @participant = Participant.find(params[:id])
+    @participant.destroy
+    authorize @participant
+  end
+
+  private
+
+  def participant_params
+    params.require(:participant).permit(:user_id, :trip_id)
+  end
 end
