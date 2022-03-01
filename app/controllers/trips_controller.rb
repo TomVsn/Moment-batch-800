ae25e9247c215c07700d951d6fa5910e39ac72c8
Pierre Coppermann <pcoppy@gmail.com>
2022-03-01 14:47:34 +0100

    create trip controller

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -4,7 +4,50 @@ class TripsController < ApplicationController
   # end
 
   def index
-    @index = Trip.all
+    @trips = Trip.all
   end
 
+  def show
+    @trip = Trip.find(params[:id])
+    @transportations = @trip.transportations
+    @messages = @trip.messages
+    @events = @trip.events
+    @participants = @trip.participants
+    authorize @trip
+  end
+
+  def new
+    @trip = Trip.new
+    authorize @trip
+  end
+
+  def create
+    @trip = Trip.new(trip_params)
+    @trip.participants << current_user
+    authorize @trip
+    if @trip.save
+      redirect_to trip_path(@trip)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @trip = Trip.find(params[:id])
+    authorize @trip
+  end
+
+  def update
+    @trip = Trip.find(params[:id])
+    @trip.update(trip_params)
+    authorize @trip
+    redirect_to trips_path
+  end
+
+  def destroy
+    @trip = Trip.find(params[:id])
+    authorize @trip
+    @trip.destroy
+    redirect_to trips_path
+  end
 end
