d9a7182ac2f6e96ac41c30c965f4f785c3648d37
Thomas Voisin <thomas.voisin08@gmail.com>
2022-03-01 11:39:25 +0100

    first set up pundit and correction has_many

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,2 +1,10 @@
 class TripsController < ApplicationController
+  # def index
+  #   @trips = policy_scope(Trip).order(created_at: :desc)
+  # end
+
+  def index
+    @index = Trip.all
+  end
+
 end
