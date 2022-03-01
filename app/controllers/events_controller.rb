6d33931137c3093651a6f85036f697a09d752e4f
Pierre Coppermann <pcoppy@gmail.com>
2022-03-01 16:22:26 +0100

    create events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,55 @@
 class EventsController < ApplicationController
+
+  def index
+    @events = Event.all
+  end
+
+  def show
+    @event = Event.find(params[:id])
+    authorize @event
+  end
+
+  def new
+    @participant = Participant.find(params[:participant_id])
+    @event = Event.new
+    authorize @event
+  end
+
+  def create
+    @event = Event.new(event_params)
+    @participant = Participant.find(params[:participant_id])
+    @event.participant = @partcipant
+    @event.event_participants << @participant
+    authorize @event
+    if @event.save
+      redirect_to event_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @event = Event.find(params[:id])
+    authorize @event
+  end
+
+  def update
+    @event = Event.find(params[:id])
+    @event.update(event_params)
+    authorize @event
+    redirect_to event_path
+  end
+
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+    authorize @event
+    redirect_to trip_path
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:description, :start_date, :end_date, :start_hour, :end_hour, :event_participants)
+  end
 end
