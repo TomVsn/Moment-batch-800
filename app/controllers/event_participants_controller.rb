e28fb9dd240680e8a7ce3a7f3df1c6cff8998e35
Pierre Coppermann <pcoppy@gmail.com>
2022-03-01 16:57:47 +0100

    create event_participants controller

--- app/controllers/event_participants_controller.rb
+++ app/controllers/event_participants_controller.rb
@@ -1,2 +1,26 @@
 class EventParticipantsController < ApplicationController
+  def new
+    @event_participant = EventParticipant.new
+    @event = Event.find(params[:event_id])
+  end
+
+  def create
+    @event_participant = EventParticipant.new(event_participant_params)
+    @event = Event.find(params[:event_id])
+    @event_participant.event = @event
+    @event_participant.save
+    redirect_to trip_path
+  end
+
+  def destroy
+    @event_participant = EventParticipant.find(params[:event_id])
+    @event_participant.destroy
+    redirect_to trip_path
+  end
+
+  private
+
+  def event_participant_params
+    params.require(:event_participant).permit(:participant_id)
+  end
 end
