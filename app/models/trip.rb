a15fb5de788d51c587c987a8e0a7454b73b47e36
Myriam Delbreil <myriam.delbreil@live.fr>
2022-03-03 11:05:54 +0100

    nv pb résolu on attend tjs accomodations

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,7 +1,7 @@
 class Trip < ApplicationRecord
   belongs_to :user
   has_many :accomodations, dependent: :destroy
-  has_many :transportations, through: :participants
+  has_many :transportations
   has_many :events, dependent: :destroy
   has_many :messages, dependent: :destroy
   has_many :participants, dependent: :destroy
