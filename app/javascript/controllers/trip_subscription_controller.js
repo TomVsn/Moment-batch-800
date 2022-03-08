9cd8d4b2f438349e06ef2a97ea38ec91226c605d
Myriam Delbreil <myriam.delbreil@live.fr>
2022-03-08 10:40:55 +0100

    pb résolu amen jpp inchallah ça marche

--- app/javascript/controllers/trip_subscription_controller.js
+++ app/javascript/controllers/trip_subscription_controller.js
@@ -10,11 +10,11 @@ export default class extends Controller {
       { channel: "TripChannel", id: this.tripIdValue },
       { received: data => this.insertMessageScrollDownAndResetForm(data)}),
       console.log(`Subscribe to the chatroom with the id ${this.tripIdValue}.`)
-}
+  }
 
-insertMessageScrollDownAndResetForm(data) {
-  this.messagesTarget.insertAdjacentHTML("beforeend", data)
-  this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
-  this.formTarget.reset()
-}
+  insertMessageScrollDownAndResetForm(data) {
+    this.messagesTarget.insertAdjacentHTML("beforeend", data)
+    this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
+    this.formTarget.reset()
+  }
 }
