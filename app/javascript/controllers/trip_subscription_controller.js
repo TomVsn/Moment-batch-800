774bffaa41b61caf49646fd35a65e4e4a938f203
Myriam Delbreil <myriam.delbreil@live.fr>
2022-03-10 12:36:40 +0100

    ne pas toucher

--- app/javascript/controllers/trip_subscription_controller.js
+++ app/javascript/controllers/trip_subscription_controller.js
@@ -6,19 +6,23 @@ export default class extends Controller {
     tripId: Number,
     currentUserId: Number
   }
-  static targets = ["messages", "form", "message"]
+  static targets = ["messages", "form", "message", "notif"]
 
   connect() {
     this.channel = consumer.subscriptions.create(
       { channel: "TripChannel", id: this.tripIdValue },
-      { received: data => this.insertMessageScrollDownAndResetForm(data)}),
-      console.log(`Subscribe to the chatroom with the id ${this.tripIdValue}.`)
-  }
+      { received: data => this.insertMessageScrollDownAndResetForm(data)},
+      console.log(`Subscribe to the chatroom with the id ${this.tripIdValue}.`),
+      console.log(this.notifTarget))
+      console.log(this.notifTarget.classList)
+    }
 
-  insertMessageScrollDownAndResetForm(data) {
-    this.messagesTarget.insertAdjacentHTML("beforeend", data.message_html);
-    if (this.currentUserIdValue != data.auhtor_id) this.messageTargets[this.messageTargets.length - 1].classList.remove('my-message');
-    this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight);
-    this.formTarget.reset();
+    insertMessageScrollDownAndResetForm(data) {
+      this.messagesTarget.insertAdjacentHTML("beforeend", data.message_html);
+      if (this.currentUserIdValue != data.auhtor_id) this.messageTargets[this.messageTargets.length - 1].classList.remove('my-message');
+      this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight);
+      this.formTarget.reset();
+      this.notifTarget.classList.remove('d-none')
+    // console.log(this.notifTarget)
   }
 }
