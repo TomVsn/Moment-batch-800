07fdbe6b52953c4424010787cded501025937c59
Myriam Delbreil <myriam.delbreil@live.fr>
2022-03-10 16:23:09 +0100

    test

--- app/controllers/accomodations_controller.rb
+++ app/controllers/accomodations_controller.rb
@@ -3,6 +3,12 @@ class AccomodationsController < ApplicationController
   def index
     @trip = Trip.find(params[:trip_id])
     @accomodations = policy_scope(@trip.accomodations)
+    @markers = @accomodation.geocoded.map do |flat|
+      {
+        lat: accomodation.latitude,
+        lng: accomodation.longitude
+      }
+    end
   end
 
   def show
